6c7e9bda0af37a2e73b3c1c2ee3e453171c8dfe6
Shuanglong <xjshuanglong@126.com>
2021-04-08 21:05:05 +0800

    准备攻克路径和问题 III

--- LeetCode/LC0437_TreePathSum_III.h
+++ LeetCode/LC0437_TreePathSum_III.h
@@ -10,15 +10,23 @@ public:
     {
         std::cout << "LC0437_TreePathSum_III --> MainEntry" << std::endl;
         // root = [10, 5, -3, 3, 2, null, 11, 3, -2, null, 1], sum = 8
-        int sum = 8;
-        TreeNode *root = nullptr;
-        std::cout << "LC0437_TreePathSum_III --> Result : " << this->pathSum(root, sum) << std::endl;
+        int sum1 = 8;
+        std::vector<int> root1Vec = { 10, 5, -3, 3, 2, INT_MIN, 11, 3, -2, INT_MIN, 1 };
+        TreeNode *root1 = LeetCodeTreeNodeCreate(root1Vec);
+        std::cout << "LC0437_TreePathSum_III --> Result : " << this->pathSum(root1, sum1) << std::endl;
         return 0;
     }
 
 public:
     int pathSum(TreeNode *root, int sum)
     {
-        return 0;
+        int result = 0;
+        std::queue<int> path;
+        this->traversalDFS(root, sum, result, path);
+        return result;
+    }
+
+    void traversalDFS(TreeNode *root, int sum, int &result, std::queue<int> &path)
+    {
     }
 };
