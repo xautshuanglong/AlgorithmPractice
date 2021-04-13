d2cfec629d2d8cbd4469fb0870c25f05931996be
JiangChuanbiao <jiangchuanbiao@genomics.cn>
2021-04-13 14:44:44 +0800

    整合 QT 中算法练习

---
+++ LeetCode/LC0122_MaxProfit_II.h
@@ -0,0 +1,20 @@
+#pragma once
+
+#include "LeetCodeDefines.h"
+#include "ILeetCodeEntry.h"
+
+class LC0121_MaxProfit : public ILeetCodeEntry
+{
+public:
+    int MainEntry() override
+    {
+        std::cout << "LC0121_MaxProfit --> MainEntry" << std::endl;
+        return 0;
+    }
+
+public:
+    int MaxProfit(std::vector<int> &prices)
+    {
+
+    }
+};
