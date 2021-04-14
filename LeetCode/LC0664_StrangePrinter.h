39fb9abcfb14b1497949a4492e24b35daae37dea
Shuanglong <xjshuanglong@126.com>
2021-04-15 01:36:03 +0800

    摘录实现

--- LeetCode/LC0664_StrangePrinter.h
+++ LeetCode/LC0664_StrangePrinter.h
@@ -32,17 +32,58 @@ public:
 
         int retValue = 0;
         size_t sLen = s.length();
-        
-        std::string noRepeatStr;
-        noRepeatStr.append(1, s[0]);
-        for (size_t i = 1; i < sLen; ++i)
-        {
-            if (s[i] != s[i - 1])
-            {
-                noRepeatStr.append(1, s[i]);
-            }
-        }
+
+        std::vector<std::vector<int>> memo = std::vector<std::vector<int>>(sLen, std::vector<int>(sLen, INT_MAX));
+        retValue = longTime(s, sLen, memo);
+        //retValue = this->dp(s, memo, 0, sLen - 1);
 
         return retValue;
+    }
+
+    int longTime(std::string &s, int sLen, std::vector<std::vector<int>> &memo)
+    {
+        for (int i = 0; i < sLen; i++)
+        {
+            memo[i][i] = 1;
+        }
+        for (int len = 1; len < sLen; len++)
+        {
+            for (int i = 0; i + len < sLen; i++)
+            {
+                memo[i][i + len] = len + 1;
+                for (int k = i; k < (i + len); k++)
+                {
+                    int total = memo[i][k] + memo[k + 1][i + len];
+                    if (s[k] == s[i + len])
+                    {
+                        total--;
+                    }
+                    memo[i][i + len] = min(total, memo[i][i + len]);
+                }
+
+            }
+        }
+        return memo[0][sLen - 1];
+    }
+
+    int dp(std::string &s, std::vector<std::vector<int>> &memo, int i, int j)
+    {
+        if (i > j)
+        {
+            return 0;
+        }
+        if (memo[i][j] == INT_MAX)
+        {
+            int tempAns = dp(s, memo, i + 1, j) + 1;
+            for (int k = i + 1; k <= j; ++k)
+            {
+                if (s[k] == s[i])
+                {
+                    tempAns = min(tempAns, dp(s, memo, i, k - 1) + dp(s, memo, k + 1, j));
+                }
+            }
+            memo[i][j] = tempAns;
+        }
+        return memo[i][j];
     }
 };
